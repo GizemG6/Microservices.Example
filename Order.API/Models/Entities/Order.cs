8b9ffb837b957b5ba280bfe8f018edbe19f19e9d
cang8 <0.gizemgunes@gmail.com>
2024-01-22 08:45:50 +0300

    add-entities

--- Order.API/Models/Entities/Order.cs
+++ Order.API/Models/Entities/Order.cs
@@ -1,6 +1,15 @@
-﻿namespace Order.API.Models.Entities
+﻿using Microsoft.AspNetCore.Connections;
+using Order.API.Models.Enums;
+
+namespace Order.API.Models.Entities
 {
     public class Order
     {
+        public Guid OrderId { get; set; }
+        public Guid BuyerID { get; set; }
+        public long TotalPrice { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
     }
 }

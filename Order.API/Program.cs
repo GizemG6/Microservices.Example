8169ba04b8c0fbc76315276186748eae112449b3
cang8 <0.gizemgunes@gmail.com>
2024-02-14 16:41:24 +0300

    added-rabbitmqyrl

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -15,7 +15,13 @@ builder.Services.AddDbContext<OrderAPIDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
 });
 
-builder.Services.AddMassTransit();
+builder.Services.AddMassTransit(configarator =>
+{
+    configarator.UsingRabbitMq((context, _configurator) =>
+    {
+        _configurator.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
 
 var app = builder.Build();
 

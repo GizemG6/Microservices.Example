9388f64c2ec357b4810f8b831ed1e449638a0de4
cang8 <0.gizemgunes@gmail.com>
2024-02-28 21:20:23 +0300

    appsetting-rabbitmq

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,3 +1,6 @@
+using MassTransit;
+using Stock.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(configarator =>
+{
+    configarator.UsingRabbitMq((context, _configurator) =>
+    {
+        _configurator.Host(builder.Configuration["RabbitMQ"]);
+    });
+});
+
+builder.Services.AddSingleton<MongoDBService>();
 
 var app = builder.Build();
 
